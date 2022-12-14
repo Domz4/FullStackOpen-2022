5d8425b4a2249a66226338c319223e30b4245eb9
Dominik <domrza99@gmail.com>
2022-12-14 13:54:21 +0100

    add redux-unicafe & redux-anecdotes(creating and voting functionality)

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,41 @@
+import { useSelector, useDispatch } from "react-redux";
+import { addVote, addAnecdote } from "./reducers/anecdoteReducer";
+const App = () => {
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  console.log(useSelector((state) => state.votes));
+  const vote = (id) => {
+    dispatch(addVote(id));
+  };
+
+  const handleAnecdote = (e) => {
+    e.preventDefault();
+    const newContent = e.target.anecdote.value;
+    e.target.anecdote.value = "";
+    dispatch(addAnecdote(newContent));
+  };
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form onSubmit={handleAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
