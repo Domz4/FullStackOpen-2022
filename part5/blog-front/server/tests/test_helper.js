ed558b498028cdb05b8a74dbb470faa80476919f
Dominik <domrza99@gmail.com>
2023-06-21 13:20:07 +0200

    cleanup

--- part5/blog-front/server/tests/test_helper.js
+++ part5/blog-front/server/tests/test_helper.js
@@ -52,10 +52,6 @@ const initialBlogs = [
   },
 ];
 
-const blogsInDb = async () => {
-  const blogs = await Blog.find({});
-  return blogs.map((blog) => blog.toJSON());
-};
 
 const usersInDb = async () => {
   const users = await User.find({});
